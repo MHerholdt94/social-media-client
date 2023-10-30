f8ae56fa4d1c6273e4b271b06425140dfd1c2774
MHerholdt94 <mathiasbherholdt@gmail.com>
2023-10-30 15:00:08 +0100

    Adds an e2e Cypress test in login_and_access_profile.spec.cy.js

--- cypress/e2e/login_and_access_profile.spec.cy.js
+++ cypress/e2e/login_and_access_profile.spec.cy.js
@@ -1,5 +1,21 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+describe("User login and profile access", () => {
+  it("logs in user and accesses profile", () => {
+    // Visit the website
+    cy.visit("http://localhost:5500");
+    cy.wait(1000);
+
+    // Click the "login" button
+    cy.get("#registerModal .modal-footer button:nth-child(2)").click();
+    cy.wait(1000);
+
+    // Fill in login credentials and submit
+    cy.get("input#loginEmail").type("MHerholdt94_test@stud.noroff.no", {
+      delay: 100,
+    });
+    cy.get("input#loginPassword").type("test1234{enter}", { delay: 100 });
+    cy.wait(1000);
+
+    // Verify the user is logged in and is on their profile page
+    cy.get("h4").contains("MHerholdt94_test");
   });
 });
