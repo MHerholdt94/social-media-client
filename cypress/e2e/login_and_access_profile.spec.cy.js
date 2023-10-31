c7f672928801fbd8df95c3b3148a268e9f160e40
MHerholdt94 <mathiasbherholdt@gmail.com>
2023-10-31 14:25:48 +0100

    Modifies login_and_access_profile.spec.cy.js with secrets

--- cypress/e2e/login_and_access_profile.spec.cy.js
+++ cypress/e2e/login_and_access_profile.spec.cy.js
@@ -1,23 +1,35 @@
 describe("User login and profile access", () => {
   it("logs in user and accesses profile", () => {
+    const apiLoginUrl = Cypress.env("secrets.API_LOGIN_URL");
+    const userEmail = Cypress.env("secrets.TEST_USER_EMAIL");
+    const userPassword = Cypress.env("secrets.TEST_USER_PASSWORD");
+
     // Visit the website
     cy.visit("http://localhost:5500");
     cy.wait(1000);
 
     // Intercept the authentication request
-    cy.intercept("https://nf-api.onrender.com/api/v1/social/auth/login").as(
-      "authLogin",
-    );
+    // cy.intercept("https://nf-api.onrender.com/api/v1/social/auth/login").as(
+    //   "authLogin"
+    // );
+
+    cy.intercept(apiLoginUrl).as("authLogin");
 
     // Click the "login" button
     cy.get("#registerModal .modal-footer button:nth-child(2)").click();
     cy.wait(1000);
 
     // Fill in login credentials and submit
-    cy.get("input#loginEmail").type("MHerholdt94_test@stud.noroff.no", {
+    // cy.get("input#loginEmail").type("MHerholdt94_test@stud.noroff.no", {
+    //   delay: 100,
+    // });
+    // cy.get("input#loginPassword").type("test1234{enter}", { delay: 100 });
+    cy.get("input#loginEmail").type(userEmail, {
       delay: 100,
     });
-    cy.get("input#loginPassword").type("test1234{enter}", { delay: 100 });
+    cy.get("input#loginPassword").type(userPassword + "{enter}", {
+      delay: 100,
+    });
     cy.wait(1000);
 
     // Wait for the authentication request to complete
